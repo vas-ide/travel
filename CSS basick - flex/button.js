2221d4fdf0d9f4b06d1403d4e02adb33bb6d99c6
vas-ide <vas.ide2022@gmail.com>
2022-07-16 15:10:51 +0300

    init: button

---
+++ CSS basick - flex/button.js
@@ -0,0 +1,19 @@
+const button = document.getElementById('button')
+const span = document.getElementById('span')
+
+const Counter = function (){
+    this.count = 0
+    this.increase = function (){
+        this.count = this.count + 1
+    }
+}
+
+const counter = new Counter()
+span.innerHTML = counter.count
+
+button.addEventListener('click', (event) => {
+    console.log(event)
+    counter.increase()
+    span.innerHTML = counter.count
+    console.log(counter.count)
+}) 

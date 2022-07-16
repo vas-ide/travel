67e7c755febde80329f7a6594d2ed3879f2f1f9e
vas-ide <vas.ide2022@gmail.com>
2022-07-16 16:48:40 +0300

    init: callback

---
+++ CSS basick - flex/callback.js
@@ -0,0 +1,19 @@
+//Для примера
+
+const callPolice = () => {
+    console.log(('Call 02'))
+}
+
+const alarm = (thieves, callback) => {
+    console.log(thieves)
+    if (thieves < 10 ) {
+        return;
+    }
+    callback(thieves)
+}
+
+for (let i = 0; i < 20; i++) {
+    setTimeout(() => {
+        alarm(i, callPolice)
+    }, 100 * i)
+}

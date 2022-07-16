a3689085e6b114228b3c130b363971509cecd435
vas-ide <vas.ide2022@gmail.com>
2022-07-16 18:45:52 +0300

    fix: recursion

--- CSS basick - flex/recursion.js
+++ CSS basick - flex/recursion.js
@@ -1,9 +1,10 @@
 
-const array = [1, 2, 3, 4, [5, 6, [7, 8]]]
-const  result = []
+const array = [1, 2, 3, 4, [5, 6, [7, 8, [9, 10, [11, 12], 13, 14, 15], 16], 17], 18]
+const result = []
 
 
 const flattenArray = function(arr) {
+    console.log(arguments)
     for (let i = 0; i < arr.length; i++) {
         const currentValue = arr[i]
     if(typeof  currentValue !== 'number') {
